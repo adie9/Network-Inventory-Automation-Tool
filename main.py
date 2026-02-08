4cddfd350f19303c728d60699b9aa8b693b49fc7
Adrian Guerra <70285960+adie9@users.noreply.github.com>
2026-02-08 16:36:21 -0500

    Add error handling for unsuccessful request

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
-import requests, json, tabulate
+import requests
+import pandas as pd
+
 def main():
-    # Get API URL
+    # Get data from API URL
     r = requests.get("https://6988ea18780e8375a6897173.mockapi.io/devices")
+    
+    # Error handling for unsuccessful data retrieval
+    if r.status_code != 200:
+        print("Error! Could not retrieve data. Exiting program...")
+        return
+    
     devices = r.json()
     
-    # Convert dictionary to JSON string
-    json_string = json.dumps(devices, indent=4)
-    print(json_string)
-    
-    
-    
+    # Create table using data from API request
+    data_frame = pd.DataFrame(devices)
+    print(data_frame) 
+      
 if __name__ == "__main__":
     main()
